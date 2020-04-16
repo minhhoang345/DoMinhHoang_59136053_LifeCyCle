0993b8216326404f7e54f4a7728ffa3f39d7a044
DELL <minhhoang345678@gmail.com>
2020-04-17 00:35:38 +0700

    DoMinhHoang_59136053_LifeCycle

--- app/src/main/java/vn/edu/ntu/dominhhoang_59136053_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/dominhhoang_59136053_lifecycle/MainActivity.java
@@ -3,12 +3,62 @@ package vn.edu.ntu.dominhhoang_59136053_lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtTG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LifeCycle","onCreate Called");
+        txtTG = findViewById(R.id.txtTG);
+        SimpleDateFormat spF = new SimpleDateFormat("HH:mm:ss");
+        String date = spF.format(new Date());
+        txtTG.setText(date);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCycle","onStart Called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LifeCycle","onRestart Called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifeCycle","onResume Called");
+        Toast.makeText(getApplicationContext(),"DoMinhHoang_59136053_LifeCycle",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCycle","onPause Called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifeCycle","onStop Called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LifeCycle","onDestroy Called");
+    }
+
 }
