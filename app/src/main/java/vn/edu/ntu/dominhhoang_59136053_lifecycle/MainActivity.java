43b0c584ce3cbe1943cae6b638f36aeac420e61d
DELL <minhhoang345678@gmail.com>
2020-04-17 16:11:03 +0700

    DoMinhHoang_59136053_LifeCycle

--- app/src/main/java/vn/edu/ntu/dominhhoang_59136053_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/dominhhoang_59136053_lifecycle/MainActivity.java
@@ -22,43 +22,49 @@ public class MainActivity extends AppCompatActivity {
         SimpleDateFormat spF = new SimpleDateFormat("HH:mm:ss");
         String date = spF.format(new Date());
         txtTG.setText(date);
+        Toast.makeText(getApplicationContext(),"onCreate",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d("LifeCycle","onStart Called");
+        Toast.makeText(getApplicationContext(),"onStart",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d("LifeCycle","onRestart Called");
+        Toast.makeText(getApplicationContext(),"onRestart",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d("LifeCycle","onResume Called");
-        Toast.makeText(getApplicationContext(),"DoMinhHoang_59136053_LifeCycle",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),"onResume",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d("LifeCycle","onPause Called");
+        Toast.makeText(getApplicationContext(),"onPause",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.d("LifeCycle","onStop Called");
+        Toast.makeText(getApplicationContext(),"onStop",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d("LifeCycle","onDestroy Called");
+        Toast.makeText(getApplicationContext(),"onDestroy",Toast.LENGTH_SHORT).show();
     }
 
 }
